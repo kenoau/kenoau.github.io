c4ebefb7d25689be166af92ca3e65742c0ae4d6b
Quinton Reeves <qreeves@gmail.com>
2024-10-02 15:21:28 +1000

    remove analytics

--- bits/js/site.js
+++ bits/js/site.js
@@ -1,12 +1,3 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','/bits/js/analytics.js','ga');
-
-ga('create', 'UA-45886576-1', 'kenoau.redeclipse.net');
-ga('require', 'linkid', 'linkid.js');
-ga('send', 'pageview');
-
 $(document).ready(function() {
     jQuery("time.timeago").timeago();
     jQuery.timeago.settings = {
